dc18b75b2eb5c23b50d4f236d14c28c7cc81a9a3
beepscore <support@beepscore.com>
2015-02-19 23:21:53 -0800

    Initial Commit

---
+++ UnicodeConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnicodeConverter
+//
+//  Created by Steve Baker on 2/19/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnicodeConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnicodeConverter
+//
+//  Created by Steve Baker on 2/19/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
